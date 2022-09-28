b697918186046b3f6feccdebba495810202eaa2e
Phumrapee Limpianchop <git@rayriffy.com>
2022-09-29 05:56:52 +0900

    chore: staged changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   manifest: {
+    //     name: 'KnuckleBones',
+    //     short_name: 'KnuckleBones'
+    //   },
+    // }),
+    svelte(),
+  ],
 })
